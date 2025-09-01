6fc8796604cb9044e698415a2d664cf418046b76
xXLuisHenriqueXx <luishenri567@gmail.com>
2025-09-01 14:22:49 -0300

    REFAC: tab bar

--- mobile/src/components/CustomTabBar/index.tsx
+++ mobile/src/components/CustomTabBar/index.tsx
@@ -2,66 +2,23 @@ import React, { memo } from "react";
 import { TouchableOpacity, View } from "react-native";
 import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import { AnimatePresence, MotiText, MotiView } from "moti";
-import {
-  Home,
-  LucideIcon,
-  PlusSquare,
-  Search,
-  UserCircle,
-} from "lucide-react-native";
-
-interface IButtonsDataProps {
-  key: string;
-  text: string;
-  label: string;
-  width: number;
-  icon: LucideIcon;
-}
+import { buttonsData } from "@src/static/TabBarButtonsData";
 
 const CustomTabBar = ({
   state,
   descriptors,
   navigation,
 }: BottomTabBarProps) => {
-  const buttonsData: IButtonsDataProps[] = [
-    { key: "home", text: "Início", label: "home", width: 40, icon: Home },
-    {
-      key: "search",
-      text: "Pesquisar",
-      label: "search",
-      width: 74,
-      icon: Search,
-    },
-    {
-      key: "create",
-      text: "Criar",
-      label: "create",
-      width: 40,
-      icon: PlusSquare,
-    },
-    {
-      key: "account",
-      text: "Conta",
-      label: "account",
-      width: 46,
-      icon: UserCircle,
-    },
-  ];
-
   return (
-    <View className="absolute bottom-6 left-6 right-6 flex-row items-center justify-between gap-x-2 p-6 bg-contrast rounded-3xl">
+    <View
+      className="absolute bottom-6 left-6 right-6 flex-row items-center justify-between gap-x-2 p-6 bg-contrast rounded-3xl"
+      style={{ elevation: 8, shadowColor: "#13131399" }}
+    >
       {state.routes.map((route, index) => {
         const { options } = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
 
         const isFocused = state.index === index;
 
-        // Match route with your button data
         const button = buttonsData.find(
           (b) => b.key === route.name.toLowerCase()
         );
@@ -97,11 +54,11 @@ const CustomTabBar = ({
                   from={{ width: 0, opacity: 0 }}
                   animate={{ width: button.width, opacity: 1 }}
                   exit={{ width: 0, opacity: 0 }}
-                  transition={{ type: "timing", duration: 350 }}
+                  transition={{ type: "timing", duration: 300 }}
                   style={{ overflow: "hidden" }}
                 >
                   <MotiText
-                    className="text-lg font-redHatDisplayMedium text-highlight"
+                    className="text-lg font-redHatDisplaySemiBold text-highlight"
                     numberOfLines={1}
                   >
                     {button.text}
