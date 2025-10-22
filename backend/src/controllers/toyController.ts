deb949390d0723142ade2b712eb80529b1942998
Arthur <arthurmorof@hotmail.com>
2025-10-22 15:26:05 -0300

    FIX: update toy route

--- backend/src/controllers/toyController.ts
+++ backend/src/controllers/toyController.ts
@@ -38,7 +38,7 @@ export const ToyController = {
   ) {
     try {
       const userId = (req as any).user.userId;
-      const { id: toyId } = req.params;
+      const { toyId } = req.params;
 
       const updatedToy = await ToyService.updateToy(toyId, req.body, userId);
 
