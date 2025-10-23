ac688086fc21bde5610ea623ee36846aa14b869d
Arthur <arthurmorof@hotmail.com>
2025-10-22 21:47:19 -0300

    FIX: added cascade delete and removed typo

--- backend/src/controllers/toyController.ts
+++ backend/src/controllers/toyController.ts
@@ -60,8 +60,8 @@ export const ToyController = {
   ) {
     try {
       const userId = (req as any).user.userId;
-      const { id: toyId } = req.params;
-
+      const { toyId } = req.params;
+      console.log(userId, toyId)
       await ToyService.deleteToy(toyId, userId);
 
       return reply
