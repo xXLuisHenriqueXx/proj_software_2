2e08c351f4b964721826ac3f90d8265a3d361de6
xXLuisHenriqueXx <luishenri567@gmail.com>
2025-10-29 09:12:27 -0300

    ADD: Chat entitie

---
+++ mobile/src/common/Entities/Chat.ts
@@ -0,0 +1,14 @@
+interface ILatestMessage {
+  id: string;
+  message: string;
+  sent_at: Date;
+  sent_by_me: boolean;
+  seen: boolean;
+}
+
+export interface IChat {
+  id: string;
+  userName: string;
+  userPicture: string;
+  latestMessage: ILatestMessage;
+}
