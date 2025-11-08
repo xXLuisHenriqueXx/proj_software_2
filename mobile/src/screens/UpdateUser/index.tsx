66a92d9e149a58e1c9af0376dad1cd2257fbf0e5
xXLuisHenriqueXx <luishenri567@gmail.com>
2025-11-08 11:02:13 -0300

    REFAC: update user for performance and ux

--- mobile/src/screens/UpdateUser/index.tsx
+++ mobile/src/screens/UpdateUser/index.tsx
@@ -1,11 +1,5 @@
-import { useEffect, useState } from "react";
-import {
-  Image,
-  Pressable,
-  ScrollView,
-  useWindowDimensions,
-  View,
-} from "react-native";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Image, Pressable, ScrollView, TextInput, View } from "react-native";
 import { styles } from "./styles";
 import Toast from "react-native-toast-message";
 import * as ImagePicker from "expo-image-picker";
@@ -25,13 +19,14 @@ import {
   BACKGROUND_SECONDARY_COLOR,
   HIGHLIGHT_COLOR,
 } from "@src/constants/Colors";
+import { WIDTH } from "@src/constants/Values";
 
 const UpdateUser = () => {
   const { appNavigation } = useAppNavigation();
-  const { width } = useWindowDimensions();
 
   const [fields, setFields] = useState<IUpdateUser>({
     name: "",
+    email: "",
     pix_key: "",
     addressDistrict: "",
     addressStreet: "",
@@ -43,14 +38,22 @@ const UpdateUser = () => {
   const [sending, setSending] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const streetWidth = width * 0.7 - 28;
-  const numberWidth = width * 0.3 - 28;
-  const neighborhoodWidth = width * 0.6 - 28;
-  const extraWidth = width * 0.4 - 28;
+  const emailRef = useRef<TextInput>(null);
+  const pixRef = useRef<TextInput>(null);
+  const cepRef = useRef<TextInput>(null);
+  const streetRef = useRef<TextInput>(null);
+  const numberRef = useRef<TextInput>(null);
+  const districtRef = useRef<TextInput>(null);
+  const detailRef = useRef<TextInput>(null);
+
+  const streetWidth = WIDTH * 0.7 - 28;
+  const numberWidth = WIDTH * 0.3 - 28;
+  const districtWidth = WIDTH * 0.6 - 28;
+  const extraWidth = WIDTH * 0.4 - 28;
 
   const [images, setImages] = useState<string>();
 
-  const pickImage = async () => {
+  const pickImage = useCallback(async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ["images"],
       allowsMultipleSelection: true,
@@ -65,35 +68,37 @@ const UpdateUser = () => {
       setImages(compressedImages[0]);
       setFields({ ...fields, picture: compressedImages[0] });
     }
-  };
+  }, []);
 
-  const compressImage = async (uri: string) => {
+  const compressImage = useCallback(async (uri: string) => {
     const manipulated = await ImageManipulator.manipulateAsync(
       uri,
       [{ resize: { width: 800 } }],
       { compress: 0.6, format: ImageManipulator.SaveFormat.JPEG, base64: true }
     );
     return `data:image/jpeg;base64,${manipulated.base64}`;
-  };
+  }, []);
 
-  const handleRemoveImage = (uri: string) => {
+  const handleRemoveImage = useCallback((uri: string) => {
     setImages(undefined);
     setFields({ ...fields, picture: "" });
-  };
+  }, []);
 
-  const handleUpdate = async () => {
+  const handleUpdate = useCallback(async () => {
     setLoading(true);
     setSending(true);
 
     try {
       const params: IUpdateUser = {
         name: fields.name,
+        email: fields.email,
         pix_key: fields.pix_key,
         addressDistrict: fields.addressDistrict,
         addressStreet: fields.addressStreet,
         addressNumber: fields.addressNumber,
         addressDetail: fields.addressDetail,
         addressCep: fields.addressCep,
+        picture: fields.picture,
       };
 
       const response = await userService.update(params);
@@ -125,9 +130,9 @@ const UpdateUser = () => {
       setLoading(false);
       setSending(false);
     }
-  };
+  }, [fields]);
 
-  const handleFetchData = async () => {
+  const handleFetchData = useCallback(async () => {
     setLoading(true);
 
     try {
@@ -136,12 +141,14 @@ const UpdateUser = () => {
       if (response) {
         const fields = {
           name: response.data.name,
+          email: response.data.email,
           pix_key: response.data.pix_key,
           addressDistrict: response.data.addressDistrict,
           addressStreet: response.data.addressStreet,
           addressNumber: response.data.addressNumber,
           addressDetail: response.data.addressDetail,
           addressCep: response.data.addressCep,
+          picture: response.data.picture,
         };
 
         setFields(fields);
@@ -155,11 +162,13 @@ const UpdateUser = () => {
     } finally {
       setLoading(false);
     }
-  };
+  }, []);
 
   useEffect(() => {
     handleFetchData();
-  }, []);
+
+    return () => {};
+  }, [handleFetchData]);
 
   if (loading && !sending) return <Loader />;
 
@@ -175,6 +184,9 @@ const UpdateUser = () => {
 
       <Pressable
         style={{
+          alignSelf: "center",
+          alignItems: "center",
+          justifyContent: "center",
           width: 128,
           height: 128,
           borderRadius: 64,
@@ -190,13 +202,13 @@ const UpdateUser = () => {
         }}
         onPress={pickImage}
       >
-        {images ? (
+        {images || fields.picture ? (
           <Image
-            source={{ uri: images }}
+            source={{ uri: images || fields.picture }}
             style={{ width: "100%", height: "100%" }}
           />
         ) : (
-          <Pencil color={HIGHLIGHT_COLOR} size={48} />
+          <Pencil color={HIGHLIGHT_COLOR} size={48} strokeWidth={1} />
         )}
       </Pressable>
 
@@ -204,61 +216,89 @@ const UpdateUser = () => {
         label="Nome"
         value={fields.name}
         onChangeText={(value) => setFields({ ...fields, name: value })}
+        returnKeyType="next"
+        onSubmitEditing={() => emailRef.current?.focus()}
       />
 
       <Input.Normal
+        ref={emailRef}
+        label="E-mail"
+        value={fields.email}
+        onChangeText={(value) => setFields({ ...fields, email: value })}
+        returnKeyType="next"
+        onSubmitEditing={() => pixRef.current?.focus()}
+      />
+
+      <Input.Normal
+        ref={pixRef}
         label="Chave Pix"
         value={fields.pix_key}
         onChangeText={(value) => setFields({ ...fields, pix_key: value })}
+        returnKeyType="next"
+        onSubmitEditing={() => cepRef.current?.focus()}
       />
 
       <Input.Masked
+        ref={cepRef}
         label="Cep"
         placeholder="00000-000"
-        returnKeyType="done"
         value={fields.addressCep}
         onChangeText={(_, unmasked) => {
           setFields({ ...fields, addressCep: unmasked });
         }}
         mask={CEP_MASK}
+        returnKeyType="next"
+        onSubmitEditing={() => streetRef.current?.focus()}
       />
 
       <View style={{ flexDirection: "row", gap: 8 }}>
         <Input.Normal
+          ref={streetRef}
           label="Rua"
           width={streetWidth}
           value={fields.addressStreet}
           onChangeText={(value) =>
             setFields({ ...fields, addressStreet: value })
           }
+          returnKeyType="next"
+          onSubmitEditing={() => numberRef.current?.focus()}
         />
 
         <Input.Normal
+          ref={numberRef}
           label="Número"
           width={numberWidth}
           value={fields.addressNumber?.toString()}
           onChangeText={(value) =>
             setFields({ ...fields, addressNumber: Number(value) })
           }
+          returnKeyType="next"
+          onSubmitEditing={() => districtRef.current?.focus()}
         />
       </View>
       <View style={{ flexDirection: "row", gap: 8 }}>
         <Input.Normal
+          ref={districtRef}
           label="Bairro"
-          width={neighborhoodWidth}
+          width={districtWidth}
           value={fields.addressDistrict}
           onChangeText={(value) =>
             setFields({ ...fields, addressDistrict: value })
           }
+          returnKeyType="next"
+          onSubmitEditing={() => detailRef.current?.focus()}
         />
 
         <Input.Normal
+          ref={detailRef}
           label="Complemento"
           width={extraWidth}
           value={fields.addressDetail}
           onChangeText={(value) =>
             setFields({ ...fields, addressDetail: value })
           }
+          returnKeyType="done"
+          onSubmitEditing={handleUpdate}
         />
       </View>
 
